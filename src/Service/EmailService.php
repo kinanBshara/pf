ed190cdca55dfeae88510ffa30f19d8b62ed80af
kinanBshara <kbshara@myges.fr>
2022-06-16 09:15:09 +0200

    adding send email service

---
+++ src/Service/EmailService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class EmailService
+{
+    public function send(string $email)
+    {
+        throw new Exception("Not yet implemented");
+    }
+}
