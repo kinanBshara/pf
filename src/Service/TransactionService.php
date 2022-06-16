2c355f4740989517e2b68f3dbf6bfcfc8176ad48
Wendy Afrim <wendy.afrim2@gmail.com>
2022-06-16 09:52:12 +0200

    Integrate method notify to send email after verifing if is time to send email

--- src/Service/TransactionService.php
+++ src/Service/TransactionService.php
@@ -9,7 +9,7 @@ class TransactionService
 {
     public const FLOOR_AMOUNT = 1000;
 
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(private EntityManagerInterface $em, private EmailService $emailService)
     {}
 
     public function debit(int $amount, UserBankAccount $userBankAccount) : int
@@ -25,7 +25,7 @@ class TransactionService
         }
 
         $this->em->flush();
-//        $this->isTimeToSendEmail();
+        $this->notify($userBankAccount->getEmail());
 
         return $amount;
     }
@@ -34,7 +34,7 @@ class TransactionService
     {
         $balance = $userBankAccount->getBalance();
 
-        if($balance + $amount > self::FLOOR_AMOUNT) {
+        if ($balance + $amount > self::FLOOR_AMOUNT) {
             $userBankAccount->setBalance(self::FLOOR_AMOUNT);
             $amount = self::FLOOR_AMOUNT - $balance;
         } else {
@@ -43,28 +43,34 @@ class TransactionService
 
         $this->em->flush();
 
-//        $this->isTimeToSendEmail();
+        $this->notify($userBankAccount->getEmail());
 
         return $amount;
-
     }
 
-    public function isTimeToSendEmail() : void
+    public function isTimeToSendEmail() : bool
     {
         $now = date('h:i A', time());
-        //$now = "11:00 pm";
-        //echo ($now);
         $current_time = \DateTime::createFromFormat('h:i a', $now);
         $start = \DateTime::createFromFormat('h:i a', "06:00 am");
         $end = \DateTime::createFromFormat('h:i a', "10:00 pm");
 
-
         if ($current_time >= $start && $current_time < $end)
         {
             echo 'don\'t send notification';
-
+            return false;
         }
+
         echo 'send notification';
+        return true;
     }
+
+    public function notify(string $email) : void
+    {
+        if (true === $this->isTimeToSendEmail()) {
+            $this->emailService->send($email);
+        }
+    }
+
 
 }
